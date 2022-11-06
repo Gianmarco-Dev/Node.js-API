74cb29e8670eba75de31871aff8ac76577eab627
Venerito Gianmarco <gianmarcodev@pm.me>
2022-11-06 16:49:58 +0100

    Setting Up resources structure

---
+++ src/resources/post/post.interface.ts
@@ -0,0 +1,7 @@
+import { Document, StringExpressionOperatorReturningBoolean } from 'mongoose';
+
+export default interface Post extends Document {
+    // want to be able to all props associates with Document, like ids, methods etc.
+    title: string;
+    body: string;
+}
